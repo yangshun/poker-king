c2af855433d57ee4ab02983459c8d64c32c8ea7f
soedar <soedar.sg@gmail.com>
2014-11-15 23:37:52 +0800

    Added getMyHand on text client

--- public/js/text.js
+++ public/js/text.js
@@ -14,7 +14,9 @@ var TextPokerForm = React.createClass({
         } else if (command[0] == "resetDeck") {
             UnaController.sendToServer("resetDeck", {}, callback);
         } else if (command[0] == "draw") {
-            UnaController.sendToServer("draw", parseInt(command[1], 10));
+            UnaController.sendToServer("draw", parseInt(command[1], 10), callback);
+        } else if (command[0] == "getMyHand") {
+            UnaController.sendToServer("getMyHand", callback);
         }
 
 
