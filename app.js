ba7866332deeae0cf50542e392dfd69d09b45be7
Chunmun <xilasirc@gmail.com>
2014-11-16 10:35:52 +0800

    Fix reset deck for discard pile

--- app.js
+++ app.js
@@ -115,6 +115,14 @@ una.server_mode.registerOnControllerInput('resetDeck',
             state.hands.__drawPile = shuffle(state.hands.__drawPile);
 
             UnaServer.sendToControllers('update', state);
+
+            UnaServer.sendToControllers('discard', {
+                cards: state.hands.__discardPile,
+                discardPile: state.hands.__discardPile,
+                playerId: "",
+                playerName: ""
+              });
+
             return { success: true }
         });
 
